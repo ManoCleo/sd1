22e88a9c46b1ddb37656e654618c6def9c6a0a01
ManoCleo <10manocleo10@gmail.com>
2026-04-09 19:48:31 -0300

    Update map coordinates and marker location

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Inicializa o mapa
-const map = L.map('map').setView([-23.561684, -46.655981], 15);
+const map = L.map('map').setView([-23.68948315184862, -46.55294977162251], 15);
 
 // Adiciona o tile layer do OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,7 +7,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Adiciona um marcador na localização da loja
-L.marker([-23.689448087131737, -46.55293897150439])
+L.marker([-23.68948315184862, -46.55294977162251])
   .addTo(map)
   .bindPopup("Faculdade Anhanguera<br>R. Atlântica, 731 - Jardim do Mar, São Bernardo do Campo - SP, 09750-480")
   .openPopup();
